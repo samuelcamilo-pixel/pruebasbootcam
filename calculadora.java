8328e086ace9b5be3760a0b8a6bb1ac2a920fe89
samuelcamilo-pixel <kmilofonck@gmail.com>
2026-05-12 21:04:39 -0500

    Add basic calculator implementation in calculadora.java
    Implement basic calculator functionality to add two numbers and display the result.

---
+++ calculadora.java
@@ -0,0 +1,18 @@
+public class calculadora {
+    public static void main (String[] args) {
+
+        //comentario de una linea
+        /* com devarias 
+        lineas sin importar*/
+
+        System.out.printf ( " --- Ejecutando - - Calculadora - - - " ) ;
+
+        double n1 = 10.5;
+
+        double n2 = 5.2;
+
+        System.out.printf ( "Resultado: %f", (n1+n2) );
+
+    }
+
+}
